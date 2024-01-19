0326e26f3f5c59d6d1d9fe791dcc2daabf5f7439
Bwizz245 <brwilliamsstl@gmail.com>
2024-01-18 22:42:10 -0600

    completed class 3 studio

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -4,7 +4,18 @@ let str = "LaunchCode";
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+let n = input.question("How many letters should be shifted? ");
+
+if (n > str.length) {
+    console.log(`${n} is too many letters! Defaulting to 3.`);
+    n = 3;
+}
+
+let newStr = str.slice(n) + str.slice(0, n);
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
+
+console.log(`${str} with ${n} letters shifted is ${newStr}`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
