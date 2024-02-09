fa18187f2975eaadc271774360dd5707d46b9bf7
Bwizz245 <brwilliamsstl@gmail.com>
2024-02-08 20:40:44 -0600

    complete class 9 studio.

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,36 @@
 
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function (num) {
+        let message = "";
+        if (num % 2 == 0) {
+            message += "Launch";
+        }
+        if (num % 3 == 0) {
+            message += "Code";
+        }
+        if (num % 5 == 0) {
+            message += " Rocks";
+        }
+
+        if (message == "") {
+            message = "Rutabagas! That doesn't work.";
+        } else {
+            message += "!";
+            message = message.trim();
+            if (message == "Launch Rocks!") {
+                message += " (CRASH!!!!)";
+            }
+        }
+
+        return message;
+    }
 }
+
+console.log(typeof launchcode.percentageCoolEmployees);
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -3,6 +3,39 @@ const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
-  
+  test("organization should be nonprofit", () => {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+  test("executiveDirector should be Jeff", () => {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+  test("percentageCoolEmployees should be 100", () => {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("programsOffered should be ", () => {
+    expect(launchcode.programsOffered).toBe["Web Development", "Data Analysis", "Liftoff"];
+  });
+
+  test("should return 'Launch!' for a number ONLY divisible by 2", () => {
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  });
+  test("should return 'Code!' for a number ONLY divisible by 3", () => {
+    expect(launchcode.launchOutput(3)).toBe("Code!");
+  });
+  test("should return 'Rocks!' for a number ONLY divisible by 5", () => {
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+  });
+  test("should return 'LaunchCode!' for a number divisible by 2 AND 3", () => {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+  });
+  test("should return 'LaunchCode!' for a number divisible by 3 AND 5", () => {
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+  });
+  test("should return 'LaunchCode!' for a number divisible by 2 AND 5", () => {
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!!)");
+  });
+  test("should return 'Rutabagas! That doesn't work.' for a number NOT divisible by 2, 3, or 5", () => {
+    expect(launchcode.launchOutput(7)).toBe("Rutabagas! That doesn't work.");
+  });
 });
